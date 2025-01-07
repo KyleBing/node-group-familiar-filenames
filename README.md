0fb3310d94a743a105159e273283904de7a13bfa
KyleBing <kylebing@163.com>
2025-01-07 17:04:41 +0800

    init

---
+++ README.md
@@ -0,0 +1,62 @@
+# 找出相似文件名
+
+找出目录中文件名相似的组合在一起
+
+## 使用说明
+1. 编辑 `find_familiar_file.js` 中的 `testFolder` 变量，改为你要操作的目录
+2. 执行 `node find_familiar_file.js` 即可
+
+## 输出结果
+```bash
+
+-----------------------------------
+     TOTAL: 87
+     GROUP: 55
+-----------------------------------
+1 (9)
+                 Windows Defender Advanced Threat Protection
+                 Windows Multimedia Platform
+                 Windows Portable Devices
+                 Windows Media Player
+                 Windows Photo Viewer
+                 Windows Defender
+                 Windows Security
+                 Windows Sidebar
+                 Windows NT
+2 (3)
+                 msedge_url_fetcher_1584_1133760379
+                 msedge_url_fetcher_1584_804413934
+                 msedge_url_fetcher_736_1357382136
+3 (22)
+                 scoped_dir11924_1103314663
+                 scoped_dir15800_1133141080
+                 scoped_dir16044_1627421158
+                 scoped_dir16796_1968424546
+                 scoped_dir10096_5045164678
+                 scoped_dir13332_557416829
+                 scoped_dir14404_732788545
+                 scoped_dir15060_826427527
+                 scoped_dir16492_580741738
+                 scoped_dir16884_711201262
+                 scoped_dir17124_657204259
+                 scoped_dir2524_1536786134
+                 scoped_dir2732_1066851961
+                 scoped_dir3644_1512056807
+                 scoped_dir8476_1126523992
+                 scoped_dir8852_1782414030
+                 scoped_dir4752_129135549
+                 scoped_dir6768_962609730
+                 scoped_dir6972_609315359
+                 scoped_dir7620_149579213
+                 scoped_dir9720_519203232
+4 (1)
+                 ModifiableWindowsApps
+5 (1)
+                 Uninstall Information
+6 (1)
+                 Application Verifier
+7 (2)
+                 Microsoft SQL Server
+                 Microsoft Office
+
+```
---
+++ find_familiar_file.js
@@ -0,0 +1,102 @@
+/**
+ * 所选目录下所有相似文件名分组显示
+ */
+
+const fs = require("node:fs")
+const path = require("node:path")
+
+const testFolder = 'C:\\Program Files'
+
+let amount = 0
+
+fs.readdir(
+    testFolder,
+    {
+        withFileTypes: false,
+    },
+    (err, files) => {
+        getFamiliarFiles(files)
+    })
+
+function getFamiliarFiles(files) {
+    amount = files.length
+    let familiarFileArray = []
+
+    let sortedPureFileNames = files.map(file => {
+        return path.basename(file, path.extname(file))  // 获取到纯文件名，without 后缀名
+    })
+        .sort((a, b) => a.length > b.length ? -1 : 0)
+
+    for (let i = 0; i < sortedPureFileNames.length; i++) {
+        const outName = sortedPureFileNames[i]
+        let currentArray = [outName]
+        for (let j = i + 1; j < sortedPureFileNames.length; j++) {
+            let innerName = sortedPureFileNames[j]
+
+            if (isFileFamiliar(outName,innerName)) {
+                // console.log(outName, innerName, j, '✓')
+                currentArray.push(innerName)
+                sortedPureFileNames.splice(j, 1)
+                j--
+            } else {
+                // console.log(outName, innerName, j)
+            }
+        }
+        familiarFileArray.push(currentArray)
+    }
+
+    printFormat(familiarFileArray)
+}
+
+function isFileFamiliar(a,b){
+    // RULE 1: 除去数字之后，剩余部分相同
+    let aAfterReplace = a.replace(/\d/g, '')
+    let bAfterReplace = b.replace(/\d/g, '')
+
+    let familiarWithoutDigit = aAfterReplace === bAfterReplace
+
+
+    // RULE 2: 前一个单词相同：空格
+    let aFirstWordSpace = a.substring(0, a.indexOf(' '))
+    let bFirstWordSpace = b.substring(0, b.indexOf(' '))
+
+    let familiarWithFirstWord = aFirstWordSpace !== '' && aFirstWordSpace === bFirstWordSpace
+
+
+    // RULE 3: 前一个单词相同：_
+    let aFirstWord_ = a.substring(0, a.indexOf('_'))
+    let bFirstWord_ = b.substring(0, b.indexOf('_'))
+
+    let familiarWithFirstWord_ = aFirstWord_ !== '' && aFirstWord_ === bFirstWord_
+
+
+    // RULE 4: 前一个单词相同：__
+    let aFirstWord__ = a.substring(0, a.indexOf('-'))
+    let bFirstWord__ = b.substring(0, b.indexOf('-'))
+
+    let familiarWithFirstWord__ = aFirstWord__ !== '' && aFirstWord__ === bFirstWord__
+
+
+    return familiarWithoutDigit ||
+        familiarWithFirstWord ||
+        familiarWithFirstWord_ ||
+        familiarWithFirstWord__
+}
+
+
+function printFormat(nameGroups){
+
+    console.log('-----------------------------------')
+    console.log(`     TOTAL: ${amount}`)
+    console.log(`     GROUP: ${nameGroups.length}`)
+    console.log('-----------------------------------')
+    nameGroups.forEach((nameArray, index) => {
+        console.log(
+            String(index + 1),
+            `(${nameArray.length})`,
+        )
+        nameArray.forEach(name => {
+            console.log('\t\t',name)
+        })
+    })
+}
